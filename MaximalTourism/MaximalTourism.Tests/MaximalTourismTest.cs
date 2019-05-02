6c47df802eab975ea36a60452e7e6f84b543a684
Jacek Glen <JacekGlen@users.noreply.github.com>
2019-05-02 21:56:33 +0100

    Initial
    Initial

---
+++ MaximalTourism/MaximalTourism.Tests/MaximalTourismTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using MaximalTourism;
+
+namespace MaximalTourism.Tests
+{
+    [TestFixture]
+    public class MaximalTourismTest
+    {
+        public const int largeTestCount = 100000;
+
+        [Test]
+        [TestCase("1 1", 1)]
+        [TestCase("1 2|7 4|7 3|5 8|1 3", 5)]
+        public void CalculatesValidExamples(string inputStr, int expectedMaxCities)
+        {
+            var sut = new MaximalTourism(inputStr.Split("|"));
+
+            var result = sut.FindMaximumConnectedCities();
+
+            Assert.AreEqual(expectedMaxCities, result);
+        }
+
+        [Test]
+        public void CalculatesWithAllDisjoint()
+        {
+            List<Tuple<int, int>> cities = new List<Tuple<int, int>>();
+
+            for (int i = 1; i <= largeTestCount; ++i)
+            {
+                cities.Add(new Tuple<int, int>(i, i));
+            }
+
+            cities.Add(new Tuple<int, int>(largeTestCount + 1, largeTestCount + 2));
+
+            var sut = new MaximalTourism(cities);
+
+            var result = sut.FindMaximumConnectedCities();
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void CalculatesAllConnected()
+        {
+            List<Tuple<int, int>> cities = new List<Tuple<int, int>>();
+
+            for (int i = 1; i <= largeTestCount; ++i)
+            {
+                cities.Add(new Tuple<int, int>(i, i + 1));
+            }
+
+            cities.Add(new Tuple<int, int>(largeTestCount + 2, largeTestCount + 3));
+
+            var sut = new MaximalTourism(cities);
+
+            var result = sut.FindMaximumConnectedCities();
+
+            Assert.AreEqual(largeTestCount + 1, result);
+        }
+
+        [Test]
+        public void CalculatesRandom()
+        {
+            List<Tuple<int, int>> cities = new List<Tuple<int, int>>();
+            cities.Add(new Tuple<int, int>(1, 2));
+            cities.Add(new Tuple<int, int>(1, 3));
+
+            var rand = new Random();
+
+            for (int i = 1; i <= largeTestCount / 10; ++i)
+            {
+                var currentCity = rand.Next(largeTestCount);
+                var citiesToConnect = rand.Next(largeTestCount / 10);
+                for (int j = 0; j < citiesToConnect; ++j)
+                {
+                    cities.Add(new Tuple<int, int>(currentCity, rand.Next(largeTestCount)));
+                }
+            }
+
+            var sut = new MaximalTourism(cities);
+
+            var result = sut.FindMaximumConnectedCities();
+
+            Assert.IsTrue(result > 2);
+        }
+    }
+}
---
+++ MaximalTourism/MaximalTourism/MaximalTourism.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MaximalTourism
+{
+    public class MaximalTourism
+    {
+        IEnumerable<CityConnection> cityConnections;
+
+        public MaximalTourism(string[] args)
+        {
+            cityConnections = args.Select(a => new CityConnection(a));
+        }
+
+        public MaximalTourism(IEnumerable<Tuple<int, int>> args)
+        {
+            cityConnections = args.Select(a => new CityConnection(a)).ToList();
+        }
+
+        public int FindMaximumConnectedCities()
+        {
+            var grouped = cityConnections
+                .GroupBy(cc => cc.First)
+                .OrderBy(gcc => gcc.Key)
+                .Select(gcc => new CityGroup()
+                {
+                    Key = gcc.Key,
+                    Connected = gcc.Select(cc => cc.Second).ToArray(),
+                    Processed = false,
+                })
+                .ToDictionary(gcc => gcc.Key, gcc => gcc);
+
+
+            var workingSet = cityConnections
+                .GroupBy(cc => cc.First)
+                .ToDictionary(gcc => gcc.Key, gcc => new CityToProcess()
+                {
+                    ConnectedCities = gcc.Select(s => s.Second).ToList()
+                });
+
+            foreach (var secondGroup in cityConnections.GroupBy(cc => cc.Second))
+            {
+                if (workingSet.ContainsKey(secondGroup.Key))
+                {
+                    workingSet[secondGroup.Key].ConnectedCities.AddRange(secondGroup.Select(sg => sg.First));
+                }
+                else
+                {
+                    workingSet[secondGroup.Key] = new CityToProcess()
+                    {
+                        ConnectedCities = secondGroup.Select(sg => sg.First).ToList()
+                    };
+                }
+            }
+
+
+
+            var connectedGroups = new List<int[]>();
+
+            foreach (var g in grouped)
+            {
+                if (g.Value.Processed)
+                {
+                    continue;
+                }
+
+                var processingStack = new Stack<int>();
+                var connectedCities = new List<int>();
+
+                processingStack.Push(g.Key);
+                connectedCities.Add(g.Key);
+
+                while (processingStack.Any())
+                {
+                    var currentCity = processingStack.Pop();
+
+                    if (grouped.ContainsKey(currentCity))
+                    {
+                        var currentGroup = grouped[currentCity];
+                        if (!currentGroup.Processed)
+                        {
+                            currentGroup.Processed = true;
+                            connectedCities.AddRange(currentGroup.Connected);
+                            foreach (var c in currentGroup.Connected)
+                            {
+                                processingStack.Push(c);
+                            }
+                        }
+                    }
+                }
+
+                connectedGroups.Add(connectedCities.Distinct().ToArray());
+            }
+
+            return connectedGroups.Select(cg => cg.Length).Max();
+        }
+
+        private class CityConnection
+        {
+            public int First { get; set; }
+            public int Second { get; set; }
+
+            public CityConnection(string line)
+            {
+                var tokens = line.Split(' ');
+                First = Int32.Parse(tokens[0]);
+                Second = Int32.Parse(tokens[1]);
+
+                if (First > Second)
+                {
+                    var tmp = First;
+                    First = Second;
+                    Second = tmp;
+                }
+            }
+
+            public CityConnection(Tuple<int, int> arg)
+            {
+                if (arg.Item1 < arg.Item2)
+                {
+                    First = arg.Item1;
+                    Second = arg.Item2;
+                }
+                else
+                {
+                    First = arg.Item2;
+                    Second = arg.Item1;
+                }
+            }
+        }
+
+
+
+        private class CityGroup
+        {
+            public int Key { get; set; }
+            public int[] Connected { get; set; }
+            public bool Processed { get; set; }
+        }
+
+        private class CityToProcess
+        {
+            public List<int> ConnectedCities;
+            public int GroupId;
+        }
+
+        public struct CityToProcessStruct
+        {
+            public int[] ConnectedCities;
+            public int GroupId;
+        }
+    }
+}
